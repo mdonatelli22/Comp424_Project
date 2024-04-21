b81ee6708daf3289bfe9dd46097fa2df32c70b93
phil-keita <phillewis16@gcc.edu>
2024-04-21 19:21:46 -0400

    PageSearch methods

--- src/main/scala/PageSearch.scala
+++ src/main/scala/PageSearch.scala
@@ -23,7 +23,11 @@ object PageSearch {
    * @return      a list of the term-frequency of the occurrences of those terms in each page in the same order given
    */
   def tf(pages: List[RankedWebPage], query: List[String]): List[Double] = {
-    List() // TODO: implement this method and remove this stub
+    // TODO: implement this method and remove this stub
+    // List of page sizes of each page in pages
+    val pageSizes = pages.map(page => page.text.toLowerCase.split("\\W+").toList.length)
+    // Combine the terms count and the page size then normalize
+    count(pages, query).zip(pageSizes).map((cnt, total) => cnt/total)
   }
 
   /**
@@ -32,6 +36,18 @@ object PageSearch {
    * @return      a list of the TF-IDF score for each page in the same order given
    */
   def tfidf(pages: List[RankedWebPage], query: List[String]): List[Double] = {
-    List() // TODO: implement this method and remove this stub
+    // TF values using prev method
+    val TFValues = tf(pages, query)
+    // Number of documents in the corpus 
+    val N = pages.size
+    // List of D values
+    val DValues = pages.map { page =>
+      val textWords = page.text.toLowerCase.split("\\W+").toList
+      (for word <- query yield if textWords.contains(word) then 1 else 0).sum.toDouble
+    }
+    // Formula in Appendix C
+    val IDFValues = DValues.map(D => log(N/(D+1)))
+    // TFIDF calculation
+    TFValues.zip(IDFValues).map((tf, idf) => tf/idf)
   }
 }
